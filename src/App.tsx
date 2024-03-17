7f18491f8fd777ea5d55ef0b991cb750edf86eb0
Shukur Huseynli <bjksukur@gmail.com>
2024-03-17 18:01:59 +0400

    add VideoCarousel

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <div className='bg-black'>
+      <Navbar />
+      <Hero/>
+      <Highlights/>
+    </div>
+  )
+}
+
+export default App
